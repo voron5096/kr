b2ecb580f33cefe4d41524586093d568ef048e56
voron5096 <70198995+voron5096@users.noreply.github.com>
2023-05-10 21:42:28 +0000

    доработка кв1

--- КВ_1/cl_base.h
+++ КВ_1/cl_base.h
@@ -8,18 +8,18 @@
 using namespace std;
 
 class cl_base {
-	string s_name; // Наименование объекта: Публичное не допускается!
-	cl_base* p_head_object; // Указатель на головной объект: Публичное не допускается!
-	vector <cl_base*> sub_objects; // Подчиненные объекты: Публичное не допускается!
-public:
+		string s_name; // Наименование объекта: Публичное не допускается!
+		cl_base* p_head_object; // Указатель на головной объект: Публичное не допускается!
+		vector <cl_base*> sub_objects; // Подчиненные объекты: Публичное не допускается!
 	
-	cl_base(cl_base* head_object, string s_name = "Base object"); // 2 параметр задаёт имя по умолчанию
-	~cl_base(); // для удаления всех подчинённых объектов
-	bool set_name(string new_name); // редактирование нового значение поля
-	string get_name(); // получение имени
-	cl_base* get_head_object(); // получение имени поля p_head_object
-	cl_base* get_sub_object(string s_name); // поиск подчинённого объекта, аргументом выступает именя которое мы ищем
-	void print_tree(); // вывод метода иерархии объекта
+	public:	
+		cl_base(cl_base* head_object, string s_name = "Base object"); // 2 параметр задаёт имя по умолчанию
+		~cl_base(); // для удаления всех подчинённых объектов
+		bool set_name(string new_name); // редактирование нового значение поля
+		string get_name(); // получение имени
+		cl_base* get_head_object(); // получение имени поля p_head_object
+		cl_base* get_sub_object(string s_name); // поиск подчинённого объекта, аргументом выступает именя которое мы ищем
+		void print_tree(); // вывод метода иерархии объекта
 };
 
 #endif
